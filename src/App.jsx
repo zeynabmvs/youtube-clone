7cc6f23baf6020eefebe8442d6ab696b5305a4f7
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2024-05-09 17:21:22 +0330

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Sidebar from "./components/Sidebar";
+import VideoDetail from "./components/VideoDetail";
+import NotFound from "./components/NotFound";
+
+const App = () => {
   return (
     <>
-      <h1 className="text-white font-roboto text-3xl ">Test heading</h1>
+      <Header />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
